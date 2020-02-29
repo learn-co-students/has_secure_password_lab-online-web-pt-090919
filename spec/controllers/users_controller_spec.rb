f13af98a76d897406b1434248895b8e55aae8636
Emil Andreasyan <emil.andreasyan@yahoo.com>
2020-02-29 04:31:54 +0000

    Done.

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,30 +2,18 @@ require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
 
-  let(:steven) {{name: 'Steven',
-            password: 'un1verse',
-            password_confirmation: 'un1verse'}}
-
-  describe 'create' do
-    it "creates a new user" do
-      post :create, params: { user: steven }
-      expect(User.last.name).to eq('Steven')
-    end
-
-    it "logs you in" do
-      post :create, params: { user: steven }
-      expect(session[:user_id]).to eq(User.last.id)
-    end
-
-    it "sets your password if the confirmation matches" do
-      post :create, params: { user: steven }
-      expect(User.last.authenticate(steven[:password])).to eq(User.last)
-    end
-
-    it "redirects you if your password and confirmation don't match" do
-      typo = steven.merge(password_confirmation: 'uni111verse')
-      post :create, params: { user: typo }
-      expect(response).to redirect_to(controller: 'users', action: 'new')
+  describe "GET #new" do
+    it "returns http success" do
+      get :new
+      expect(response).to have_http_status(:success)
     end
   end
+
+  describe "GET #create" do
+    it "returns http success" do
+      get :create
+      expect(response).to have_http_status(:success)
+    end
+  end
+
 end
